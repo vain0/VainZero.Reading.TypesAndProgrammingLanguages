21b765b4d3611a3cd2ee24b5e972f1a7857bf0c5
vain0x <vainzerox@gmail.com>
2017-02-18 10:48:52 +0900

    Repl

--- scala/src/main/scala/vain0/tapl/untyped/Repl.scala
+++ scala/src/main/scala/vain0/tapl/untyped/Repl.scala
@@ -1,7 +1,21 @@
 package vain0.tapl.untyped
 
+import io._
+
 object Repl {
+  def repl(): Unit = {
+    StdIn.readLine() match {
+      case null | "quit" | "exit" =>
+        ()
+      case "" =>
+        repl()
+      case line =>
+        println(line + "!")
+        repl()
+    }
+  }
+
   def main(args: Array[String]): Unit = {
-    println("hello!")
+    repl()
   }
 }
